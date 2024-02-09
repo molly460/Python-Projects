1acc4a917a5cff6f55532c152e549bfd6ed2abce
Molly Vanessa <77613113+molly460@users.noreply.github.com>
2024-02-09 01:09:45 +0100

    Add files via upload
    Uploaded a new file.

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+car = input("Enter your fav car : ")
+color = input("Enter your fav color :")
+fav_celebrity = input("Enter your favorite celebrity: ")
+
+print("My fav car is a " + car + ",")
+print("And i love the "  + color + " one" + ".")
+print("I will love to have one like " + fav_celebrity+ "." )
