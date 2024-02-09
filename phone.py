15a5dfffc418a529d381a86e8993d70a6439d1c5
Molly Vanessa <77613113+molly460@users.noreply.github.com>
2024-02-09 01:07:04 +0100

    Add files via upload
    Uploaded a new file

---
+++ phone.py
@@ -0,0 +1,8 @@
+from iphone import phone
+
+iphone1 = phone("iphone", "ios", "iphoneXs", 17.4)
+
+samsung = phone("samsung galaxy ultra","android","S23 ultra",14)
+
+print(samsung.modelname)
+         
