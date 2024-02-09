5e841f6753ad5af37bdb83891e85f3bb341f01b7
Molly Vanessa <77613113+molly460@users.noreply.github.com>
2024-02-09 01:05:49 +0100

    Add files via upload
    Uploaded a new file.

---
+++ iphone.py
@@ -0,0 +1,7 @@
+class phone:
+
+    def __init__(self, name, operatingsystem, modelname, softwareversion, ):
+        self.name = name
+        self.operatingsystem = operatingsystem
+        self.modelname = modelname
+        self.softwareversion = softwareversion 
